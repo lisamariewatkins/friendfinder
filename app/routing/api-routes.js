68d74647c6401a8c88710699699bc76470667af6
Lisa Watkins <Lisa Watkins>
2016-07-20 14:54:03 -0500

    added get/post for api

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,12 @@
 //Routes
 
 app.get('/api/friends', function(req,res){
-	
+	res.json();
+})
+
+app.post('/api/friends', function(req,res){
+	var newFriend = req.body;
+	newFriend.friendID = newFriend.friendName.replace(/\s+/g, '').toLowerCase();
+	friends.push(newFriend);
+	res.json(newFriend);
 })
