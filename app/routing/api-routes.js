85fd3bf4ce919d09667774f2649474c3ced000c6
Lisa Watkins <Lisa Watkins>
2016-07-21 15:21:59 -0500

    fiddled with post

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,4 @@
 //Routes
-
 app.get('/api/friends', function(req,res){
 	res.json();
 })
@@ -8,5 +7,6 @@ app.post('/api/friends', function(req,res){
 	var newFriend = req.body;
 	newFriend.friendID = newFriend.friendName.replace(/\s+/g, '').toLowerCase();
 	friends.push(newFriend);
-	res.json(newFriend);
+	//write function to show best results
+	
 })
