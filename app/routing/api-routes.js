c2163fa4f6d3eeac7e3d213c5905b998e8631f8f
Lisa Watkins <Lisa Watkins>
2016-07-22 14:37:00 -0500

    working logic behind match maker

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,14 @@
+var array = require('./../data/friends.js');
+
 //Routes
 module.exports = function(app){
+
+	function add(a,b){
+		return a + b;
+	}
+
+	var bestMatch;
+
 	app.get('/api/friends', function(req,res){
 		res.json();
 	})
@@ -7,8 +16,29 @@ module.exports = function(app){
 	app.post('/api/friends', function(req,res){
 		var newFriend = req.body;
 		newFriend.friendID = newFriend.friendName.replace(/\s+/g, '').toLowerCase();
-		friends.push(newFriend);
+		array.push(newFriend);
 		//write function to show best results
+		for (var i = 0; i < friends.length; i++){
+			var differences = [];
+			var totalDifference;
+			var lastDif = 50;
+
+			for (var j = 0; j < friends[i].scores.length;j++){	
+				differences.push(Math.abs(userData.scores[j] - friends[i].scores[j]));
+				
+				totalDifference = differences.reduce(add, 0);
+				
+				if (totalDifference < lastDif){
+					lastDif = totalDifference;
+					bestMatch = friends[i];
+				}
+			}	
+		};
 	});
+
 };
+
+
+
+
 
