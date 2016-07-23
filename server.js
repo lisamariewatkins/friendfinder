03d7f7356290ad95da2d690e01585ceaca2973b8
Lisa Watkins <Lisa Watkins>
2016-07-23 14:23:02 -0500

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //SETS UP DATA PARSING
 app.use(bodyParser.json());
